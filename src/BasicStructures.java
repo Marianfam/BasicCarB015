47457a406fc6fcfac1c214bca02ae2aaedfe5be0
marian fam <m.golgos@gmail.com>
2023-03-31 22:15:02 -0400

    the baisc structur of the car is now ready

---
+++ src/BasicStructures.java
@@ -0,0 +1,6 @@
+public class BasicStructures {
+
+     String model;
+     String power;
+     String color;
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        BasicStructures audi = new BasicStructures();
+        audi .color="balck";
+        audi.model = "A3";
+        audi.power = "5000cc";
+
+    }
+}
